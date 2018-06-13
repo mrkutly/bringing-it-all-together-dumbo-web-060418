f3e228b04876a03469eb83c230b608126109625d
mark sauer-utley <mark.utley.713@gmail.com>
2018-06-13 11:43:06 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,9 +76,11 @@ class Dog
     dog
   end
 
+
   def self.new_from_db(row)
     Dog.new({id: row[0], name: row[1], breed: row[2]})
   end
+
 
   def assign_attributes(attributes)
     attributes.each do |key, value|
